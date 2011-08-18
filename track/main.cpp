14d5d8d92f1a69548555752ceaeb31ba995811e1
Stefan <stefan_reinke@gmx.de>
2011-08-18 09:16:54 +0200

    running glut/osg test version

--- track/main.cpp
+++ track/main.cpp
@@ -1,8 +1,77 @@
-#include <QtCore/QCoreApplication>
+//#include <QtCore/QCoreApplication>
+
+//#include <OpenSG/OSGQT4WindowBase.h>
+#include <OpenSG/OSGConfig.h>
+#include <OpenSG/OSGGLUT.h>
+#include <OpenSG/OSGGLUTWindow.h>
+#include <OpenSG/OSGSimpleGeometry.h>
+#include <OpenSG/OSGSimpleSceneManager.h>
+
+
+
+#include <iostream>
+
+OSG_USING_NAMESPACE
+
+OSG::SimpleSceneManager* mgr;
+using namespace std;
+int setupGLUT(int *argc, char *argv[]);
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+  //  QCoreApplication a(argc, argv);
 
-    return a.exec();
+    OSG::osgInit(argc,argv);
+    {
+	int winid = setupGLUT(&argc,argv);
+	OSG::GLUTWindowRecPtr gwin = OSG::GLUTWindow::create();
+	gwin->setGlutId(winid);
+	gwin->init();
+
+	// This will be our whole scene for now : an incredible torus
+	OSG::NodeRecPtr scene = OSG::makeTorus(.5, 2, 16, 16);
+
+	// Create and setup our little friend - the SSM
+	mgr = new OSG::SimpleSceneManager;
+	mgr->setWindow(gwin);
+	mgr->setRoot(scene);
+	mgr->showAll();
+
+
+	cout<<"wooooaaaaaahhh";
+    }
+    glutMainLoop();
+
+    //return a.exec();
+    return 0;
+}
+
+// react to size changes
+void reshape(int w, int h)
+{
+    mgr->resize(w, h);
+    glutPostRedisplay();
+}
+
+// just redraw our scene if this GLUT callback is invoked
+void display(void)
+{
+    mgr->redraw();
+}
+
+
+int setupGLUT(int *argc, char *argv[])
+{
+    glutInit(argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
+
+   // glutInitDisplayMode(GLUT_RGB);
+
+    int winid = glutCreateWindow("OpenSG First Application");
+
+    // register the GLUT callback functions
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+
+    return winid;
 }
--- track/track.pro
+++ track/track.pro
@@ -8,11 +8,41 @@ QT       += core
 
 QT       -= gui
 
+#ADDING GLUT FRAMEWORK
+QMAKE_LFLAGS += -F/System/Library/Frameworks/
+LIBS += -framework GLUT
+
+#ADDING OSG LIBS
+LIBS += /usr/local/lib/libOSGEffectGroups.dylib
+LIBS += /usr/local/lib/libOSGBase.dylib
+LIBS += /usr/local/lib/libOSGFileIO.dylib
+LIBS += /usr/local/lib/libOSGCluster.dylib
+LIBS += /usr/local/lib/libOSGGroup.dylib
+LIBS += /usr/local/lib/libOSGContribBackgroundLoader.dylib
+LIBS += /usr/local/lib/libOSGImageFileIO.dylib
+LIBS += /usr/local/lib/libOSGContribCSM.dylib
+LIBS += /usr/local/lib/libOSGState.dylib
+LIBS += /usr/local/lib/libOSGContribCSMSimplePlugin.dylib
+LIBS += /usr/local/lib/libOSGSystem.dylib
+LIBS += /usr/local/lib/libOSGContribCgFX.dylib
+LIBS += /usr/local/lib/libOSGText.dylib
+LIBS += /usr/local/lib/libOSGContribComputeBase.dylib
+LIBS += /usr/local/lib/libOSGUtil.dylib
+LIBS += /usr/local/lib/libOSGContribGUI.dylib
+LIBS += /usr/local/lib/libOSGWindow.dylib
+LIBS += /usr/local/lib/libOSGContribPLY.dylib
+LIBS += /usr/local/lib/libOSGWindowCarbon.dylib
+LIBS += /usr/local/lib/libOSGContribTrapezoidalShadowMaps.dylib
+LIBS += /usr/local/lib/libOSGWindowCocoa.dylib
+LIBS += /usr/local/lib/libOSGDrawable.dylib
+LIBS += /usr/local/lib/libOSGWindowCoreGL.dylib
+LIBS += /usr/local/lib/libOSGDynamics.dylib
+LIBS += /usr/local/lib/libOSGWindowGLUT.dylib
+
 TARGET = track
 CONFIG   += console
 CONFIG   -= app_bundle
 
 TEMPLATE = app
-
 
 SOURCES += main.cpp
